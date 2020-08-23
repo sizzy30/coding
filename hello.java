e4dc327c59c3ee967f9d5cfbe8211cd4da4ab2c9
sizzy30 <70087826+sizzy30@users.noreply.github.com>
2020-08-23 13:39:05 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class findAverage {
+static int average(int a,int b,int c) {
+	int sum=a+b+c;
+	int res= sum/3;
+	return res; 
+}
+	public static void main(String[] args) {
+  int x=2,y=4,z=5,res=0;
+	average(x,y,z);
+	System.out.println(res);
+	}
+}
